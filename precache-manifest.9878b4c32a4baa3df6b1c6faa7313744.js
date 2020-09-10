22318cf581756fd52456758935387dd03c286f48
Siri Arnesen <siri.arnesen@ntnu.no>
2020-09-10 18:00:31 +0200

    Updates

---
+++ precache-manifest.9878b4c32a4baa3df6b1c6faa7313744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8c2922efc0113fe25cff05009d7e01",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "6d152dd3505aa026e4b2",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "649ee699f7ec79736c4e",
+    "url": "/infoskjerm/static/js/2.c58ec73a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.c58ec73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d152dd3505aa026e4b2",
+    "url": "/infoskjerm/static/js/main.213b21aa.chunk.js"
+  },
+  {
+    "revision": "98c594f5279a3907d7f3",
+    "url": "/infoskjerm/static/js/runtime-main.ad5f6e6d.js"
+  }
+]);
